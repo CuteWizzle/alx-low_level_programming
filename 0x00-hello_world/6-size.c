06c2243fc203a860b96157c6dace17cc0af2682c
CuteWizzle <caleb.rj360@gmail.com>
2022-12-08 14:49:54 -0800

    Correction on 6-size.c file

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,11 +1,11 @@
 #include <stdio.h>
 /**
- * main - Entry
- * Return: Always 0
+ * main - prints the size of various types
+ * Return: 0 if executed properly, non-zero otherwise
  */
 int main(void)
 {
-	printf("Size of char: %d byte(s)\n", sizeof(char));
+	printf("Size of a char: %d byte(s)\n", sizeof(char));
 	printf("Size of an int: %d byte(s)\n", sizeof(int));
 	printf("Size of a long int: %d byte(s)\n", sizeof(long int));
 	printf("Size of a long long int: %d byte(s)\n", sizeof(long long int));
