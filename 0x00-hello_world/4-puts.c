ec012f123a1c3072b0be9f9f06da133396a5b7f9
CuteWizzle <caleb.rj360@gmail.com>
2022-12-08 13:51:10 -0800

    Correcting my code

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,9 +1,10 @@
+#include <stdio.h>
 /**
  * main-Entry
  * Return: Always 0
  */
 int main(void)
 {
-	puts("\"Programming is like building a multilingual puzzle);
+	puts("\"Programming is like building a multilingual puzzle");
 	return (0);
 }
