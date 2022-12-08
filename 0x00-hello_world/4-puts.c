d52ba1d3e18274222266f26945164d9704b42701
CuteWizzle <caleb.rj360@gmail.com>
2022-12-08 13:34:27 -0800

    Writing a program that prints exactly Programming is like building multilingual puzzle using the puts function

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,10 +1,9 @@
-#!/bin/bash
 /**
  * main-Entry
  * Return: Always 0
  */
 int main(void)
 {
-puts("\"Programming is like building a multi lingual puzzle);
-return (0);
+	puts("\"Programming is like building a multilingual puzzle);
+	return (0);
 }
