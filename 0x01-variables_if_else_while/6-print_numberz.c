485a432c37320e659af90d8f62fa52dafa2fa882
CuteWizzle <caleb.rj360@gmail.com>
2022-12-09 05:14:38 -0800

    Print integers in base 10 fro 0 followed by new line using putchar

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+ * main - Prints all single digits of base 10 from 0
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int n;
+
+	for (n = 48; n < 58; n++)
+	{
+		putchar(n);
+	}
+	putchar('\n');
+	return (0);
+}
