f916609fa0275aac200b00c218e121ab8f8bf3e4
CuteWizzle <caleb.rj360@gmail.com>
2022-12-09 05:01:31 -0800

    Print digits 0-9 in new lines

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+/**
+ * main - Prints single numbers in a new line from 0-10
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int n;
+
+	for (n = 0; n < 10; n++)
+		printf("%d", n);
+	printf("\n");
+	return (0);
+}
