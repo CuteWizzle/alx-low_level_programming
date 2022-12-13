41c02b41c1cfa80d793c7e6e76c039e3c3268511
CuteWizzle <caleb.rj360@gmail.com>
2022-12-13 07:56:43 -0800

    Using for loop

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * print_alphabets - print alphabets in lowercase and new line
+ */
+
+void print_alphabet(void);
+{
+	char la;
+
+	for (la = 'a'; la <= 'z'; la++)
+		_putchar(la);
+	_putchar('\n');
+}
