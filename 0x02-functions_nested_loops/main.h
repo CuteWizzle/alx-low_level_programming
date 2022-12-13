3c485cb67e6b9a73723cbadd4fcccfd655549019
CuteWizzle <caleb.rj360@gmail.com>
2022-12-13 07:07:44 -0800

    Program on functions and nested loops

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -1,3 +1,6 @@
+#ifndef MAIN_H
+#define MAIN_H
+
 int _putchar(char c);
 void print_alphabet(void);
 void print_alphabet_x10(void);
@@ -10,4 +13,6 @@ void jack_bauer(void);
 void times_table(void);
 int add(int, int);
 void print_to_98(int n);
-void print-times_table(int n);
+void print_times_table(int n);
+
+#endif
