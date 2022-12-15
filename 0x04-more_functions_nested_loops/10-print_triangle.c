a46bb2aa36777be7b441cd77da2c3a10f7e5c395
CuteWizzle <caleb.rj360@gmail.com>
2022-12-15 14:33:19 -0800

    Debugging file 10-print_triangle.c

--- 0x04-more_functions_nested_loops/10-print_triangle.c
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -4,7 +4,7 @@
  * print_triangle - prints a triangle, followed by a new line
  * @size: size of the triangle
  */
-void print_triangle( int size)
+void print_triangle(int size)
 {
 	if (size <= 0)
 	{
