a8526b0762c302ef81a708b9e7e7fdf18bf522ce
CuteWizzle <caleb.rj360@gmail.com>
2022-12-19 07:32:26 -0800

    task 100-atoi.c

--- 0x05-pointers_arrays_strings/100-atoi.c
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -11,13 +11,14 @@ int _atoi(char *s)
 	unsigned int num = 0;
 	int sign = 1;
 
-	do	{
-			if (*s == '-')
-				sign *= -1;
-			else if (*s >= '0' && *s <= '9')
-				num = (num * 10) + (*s - '0');
-			else if (num > 0)
-				break;
-		}	while (*s++);
-			return (num * sign);
+do	{
+	if (*s == '-')
+		sign *= -1;
+	else if (*s >= '0' && *s <= '9')
+		num = (num * 10) + (*s - '0');
+	else if (num > 0)
+		break;
+	}
+	while (*s++)
+	return (num * sign);
 }
