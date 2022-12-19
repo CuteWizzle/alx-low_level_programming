41d58613aed424f79ef93480f96a4ae972afd254
CuteWizzle <caleb.rj360@gmail.com>
2022-12-19 06:27:53 -0800

    Task 7

---
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,26 @@
+i#include "main.h"
+
+/**
+*puts_half - prints half of a string
+*followed by a new line
+*@str: input
+*/
+
+void puts_half(char *str)
+{
+	int l, n;
+
+	for (l = 0; str[l] != '\0'; ++l)
+		;
+	if (l % 2 == 0)
+	{
+		for (n = l / 2; str[n] != '\0'; ++n)
+			_putchar(str[n]);
+	}
+	else
+	{
+		for (n = ((l - 1) / 2) + 1; str[n] != '\0'; ++n)
+			_putchar(str[n]);
+	}
+	_putchar('\n');
+}
