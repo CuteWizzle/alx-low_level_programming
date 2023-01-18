85f0cacd1273308dedb8c52846c03e6c1ca7c41d
CuteWizzle <caleb.rj360@gmail.com>
2023-01-18 03:34:43 -0800

    What's my name : A function that prints a name.
    Prototype: void print_name(char *name, void (*f)(char *));
    Compile the code this way: gcc -Wall -pedantic -Werror -Wextra -std=gnu89 main/0-main.c 0-print_name.c -o a

--- 0x0F-function_pointers/0-print_name.c
+++ 0x0F-function_pointers/0-print_name.c
@@ -1,14 +1,12 @@
 #include "function_pointers.h"
 
 /**
- * print_name - function to print name
+ * print_name - prints a name.
+ * @name: input name.
+ * @f: function pointer.
  *
- * @name: name to print
- * @f: function pointer that doesn't return anything
- *
- * Return: Always 0 (Success)
-*/
-
+ * Return: no return.
+ */
 void print_name(char *name, void (*f)(char *))
 {
 	if (name && f)
